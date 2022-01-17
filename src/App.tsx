458daf4829399ff26ca91675625caeebab4bf734
Nemanja <nemanja.vasic@codecentric.de>
2022-01-17 21:58:08 +0100

    Introduce Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import { Routes, Route } from "react-router-dom";
+import Index from "./pages/Index";
+import LevelOne from "./pages/LevelOne";
+
 import "./App.css";
+import LevelTwo from "./pages/LevelTwo";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        {/* <Route path="/" element={  } /> */}
+        <Route path="/" element={<Index />} />
+        <Route path="/level-one" element={<LevelOne />} />
+        <Route path="/" element={<LevelTwo />} />
       </Routes>
     </div>
   );
