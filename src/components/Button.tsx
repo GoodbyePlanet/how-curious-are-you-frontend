145bc3c999efa9ae5fa70239ea2aa5538e8f9182
Nemanja <nemanja.vasic@codecentric.de>
2022-01-17 22:43:55 +0100

    Add custom Button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,7 @@
+import "./Button.css";
+
+function Button(): JSX.Element {
+  return <button className="submit" type="submit">Submit</button>;
+}
+
+export default Button;
--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,19 +1,29 @@
 import { useState } from "react";
+import Button from "../components/Button";
 import Input from "../components/Input";
 
 function Index() {
   const [flagOne, setFlagOne] = useState("");
+
+  const handleFlagSubmit = (event: any): void => {
+    event.preventDefault();
+
+    console.log("FLAG-ONE", flagOne);
+  }
 
   return (
     <div>
       <p>Hi there, I'm Nemanja and I have a question for you</p>
       <h3>How curious are you?</h3>
 
-      <Input
-        autoFocus
-        value={flagOne}
-        onChange={(value) => setFlagOne(value)}
-      />
+      <form onSubmit={handleFlagSubmit}>
+        <Input
+          autoFocus
+          value={flagOne}
+          onChange={(value) => setFlagOne(value)}
+        />
+        <Button />
+      </form>
     </div>
   );
 }
