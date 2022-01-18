4425d0c888d58ef5d28ac72571839526c57cfa28
Nemanja <nemanja.vasic@codecentric.de>
2022-01-18 11:35:07 +0100

    Add level NINE

---
+++ src/pages/LevelNine.tsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Button from "../components/Button";
+import Input from "../components/Input";
+
+function LevelNine() {
+  const [flagTen, setFlagTen] = useState("");
+  const navigate = useNavigate();
+
+  const handleFlagSubmit = (event: any): void => {
+    event.preventDefault();
+
+    if (flagTen === process.env.LEVEL_TEN) {
+      navigate("end");
+    }
+  };
+
+  return (
+    <div>
+      <p>Congrats, you've reached level NINE</p>
+
+      <form onSubmit={handleFlagSubmit}>
+        <Input autoFocus value={flagTen} onChange={setFlagTen} />
+        <Button />
+      </form>
+    </div>
+  );
+}
+
+export default LevelNine;
