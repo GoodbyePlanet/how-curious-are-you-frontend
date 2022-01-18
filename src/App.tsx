c72524249edc69d548dffd238988ddde09834310
Nemanja <nemanja.vasic@codecentric.de>
2022-01-18 11:31:40 +0100

    Add routes

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,10 @@ import LevelThree from "./pages/LevelThree";
 import LevelFour from "./pages/LevelFour";
 
 import "./App.css";
+import LevelFive from "./pages/LevelFive";
+import LevelSix from "./pages/LevelSix";
+import LevelSeven from "./pages/LevelSeven";
+import LevelEight from "./pages/LevelEight";
 
 function App() {
   return (
@@ -16,6 +20,10 @@ function App() {
         <Route path="/level-two" element={<LevelTwo />} />
         <Route path="/level-three" element={<LevelThree />} />
         <Route path="/level-four" element={<LevelFour />} />
+        <Route path="/level-five" element={<LevelFive />} />
+        <Route path="/level-six" element={<LevelSix />} />
+        <Route path="/level-seven" element={<LevelSeven />} />
+        <Route path="/level-eight" element={<LevelEight />} />
       </Routes>
     </div>
   );
