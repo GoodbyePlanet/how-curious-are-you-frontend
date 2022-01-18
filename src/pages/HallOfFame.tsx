f3d42cf0297e525dc45e52220dba421a1befc5ae
Nemanja <nemanja.vasic@codecentric.de>
2022-01-18 12:05:02 +0100

    List all finishers

---
+++ src/pages/HallOfFame.tsx
@@ -0,0 +1,25 @@
+interface Finisher {
+  place: number;
+  name: string;
+  socialNetworkUrl: string;
+}
+
+function HallOfFame() {
+  const finishersList = [
+    { place: 1, name: "Pero", socialNetworkUrl: "https://pero.com/" },
+    { place: 2, name: "Jovan", socialNetworkUrl: "https://jovan.com/" },
+    { place: 3, name: "Jovica", socialNetworkUrl: "https://jovica.com/" },
+  ] as Finisher[];
+
+  console.log(finishersList);
+  return (
+    <div>
+      <h3>HALL OF FAME</h3>
+      {finishersList.map((finisher) => (
+        <div key={finisher.place}>{finisher.place}. <a href={finisher.socialNetworkUrl}>{finisher.name}</a> </div>
+      ))}
+    </div>
+  );
+}
+
+export default HallOfFame;
